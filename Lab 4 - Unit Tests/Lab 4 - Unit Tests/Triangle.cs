2f66c1f7722380edb7f581181571e8ce9a627667
auhpriem <cacha51484@mail.ru>
2018-09-28 21:15:31 +0300

    Lab 4 - Unit Tests

---
+++ Lab 4 - Unit Tests/Lab 4 - Unit Tests/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4___Unit_Tests
+{
+    class Triangle
+    {
+        public static bool CheckSides(float s1, float s2, float s3)
+        {
+            var sides = new float[3] { s1, s2, s3 };
+            if (sides.Min() <= 0 || float.IsInfinity(s1 + s2 + s3))
+                return false;
+            return 2 * sides.Max() < s1 + s2 + s3;
+        }
+    }
+}
