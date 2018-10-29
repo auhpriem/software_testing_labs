8e2e5fa3150db43df1461745324db15ab505d654
auhpriem <cacha51484@mail.ru>
2018-10-29 17:52:06 +0300

    Lab 6 - PageObject/Factory Pattern

---
+++ Lab 6 - PageObjectPattern/Lab 6 - PageObjectPattern/PageObject.cs
@@ -0,0 +1,132 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6___PageObjectPattern
+{
+    class InitialPage
+    {
+        private IWebDriver driver;
+
+        public InitialPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "main_form")]
+        public IWebElement MainForm;
+
+        [FindsBy(How = How.Id, Using = "departure")]
+        public IWebElement Departure;
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='from']")]
+        public IList<IWebElement> FromPoints;
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='to']")]
+        public IList<IWebElement> ToPoints;
+
+        public void ValidateToPoints(string city)
+        {
+            foreach (var point in ToPoints)
+            {
+                try
+                {
+                    string cityAttribute = point.FindElement(By.ClassName("city")).Text;
+                    if (cityAttribute != string.Empty)
+                    {
+                        Assert.IsTrue(cityAttribute.Contains(city));
+                    }
+                }
+                catch   {    }
+            }
+        }
+        public void ValidateFromPoints(string date, string city)
+        {
+            foreach (var point in FromPoints)
+            {
+                try
+                {
+                    string cityAttribute = point.FindElement(By.ClassName("city")).Text;
+                    if (cityAttribute != string.Empty)
+                    {
+                        Assert.IsTrue(cityAttribute.Contains(city));
+                        string dateAttribute = point.GetAttribute("data-departure-time");
+                        Assert.IsTrue(dateAttribute.Contains(date));
+                    }
+                }
+                catch
+                {
+
+                }
+
+            }
+        }
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("https://www.biletik.aero/");
+        }
+    }
+
+    class FormObject
+    {
+        private IWebElement MainForm;
+
+        public FormObject(IWebElement MainForm)
+        {
+            this.MainForm = MainForm;
+            PageFactory.InitElements(MainForm, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "departure")]
+        public IWebElement Departure;
+
+        [FindsBy(How = How.ClassName, Using = "btn")]
+        public IWebElement SubmitButton;
+
+        [FindsBy(How = How.Id, Using = "arrival")]
+        public IWebElement Arrival;
+
+        [FindsBy(How = How.Id, Using = "main_datepiker_to")]
+        public IWebElement MainDatepikerTo;
+
+        [FindsBy(How = How.ClassName, Using = "count-pass")]
+        public IWebElement CountPass;
+    }
+
+    class PassengersInput
+    {
+        private IWebElement PassengerInput;
+
+        public PassengersInput(IWebElement PassengerInput)
+        {
+            this.PassengerInput = PassengerInput;
+            PageFactory.InitElements(PassengerInput, this);
+        }
+
+        [FindsBy(How = How.ClassName, Using = "widget_input")]
+        public IWebElement CountPassWidgetInput;
+
+        [FindsBy(How = How.ClassName, Using = "counter-input")]
+        public IList<IWebElement> CountPassInputs;
+
+        public void PlusAdult()
+        {
+            foreach (var element in CountPassInputs)
+            {
+                try
+                {
+                    element.FindElement(By.Name("ADT"));
+                    element.FindElement(By.ClassName("plus")).Click();
+                    break;
+                }
+                catch { }
+            }
+        }
+    }
+}
---
+++ Lab 6 - PageObjectPattern/Lab 6 - PageObjectPattern/TestCases.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Lab_6___PageObjectPattern
+{
+    [TestFixture]
+    class TestCases
+    {
+        // IWebDriver driver;
+
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            //"https://www.biletik.aero/"
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void cleanup()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void SearchWithValidData()
+        {
+            string FromPoint = "Москва";
+            string ToPoint = "Минск";
+            string date = "11.11.2018";
+            string dateFormat = "2018/11/11";
+
+            InitialPage page = new InitialPage(driver);
+            page.goToPage();
+            FormObject MainForm = new FormObject(page.MainForm);
+
+            MainForm.Departure.Clear();
+            MainForm.Departure.SendKeys(FromPoint);
+            Thread.Sleep(1000);
+            MainForm.Departure.SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+            
+            MainForm.Arrival.Clear();
+            MainForm.Arrival.SendKeys(ToPoint);
+            Thread.Sleep(1000);
+            MainForm.Arrival.SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+            
+            MainForm.MainDatepikerTo.Clear();
+            MainForm.MainDatepikerTo.SendKeys(date);
+
+            //Adults = 2
+            PassengersInput passengersInput = new PassengersInput(MainForm.CountPass);
+            passengersInput.CountPassWidgetInput.Click();
+            passengersInput.PlusAdult();
+            passengersInput.PlusAdult();
+
+            MainForm.SubmitButton.Submit();
+
+            if (!driver.Url.Contains("https://www.biletik.aero"))
+                driver.Navigate().GoToUrl("https://www.biletik.aero/result/?formtype=simple&departure=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2C+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F+(MOW)&departure_code=MOW&arrival=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA-2%2C+%D0%91%D0%B5%D0%BB%D0%BE%D1%80%D1%83%D1%81%D1%81%D0%B8%D1%8F+(MSQ)&arrival_code=MSQ&date_from="+date+"&date_come=&ADT=2&CNN=0&INF=0&INS=0&SRC=0&YTH=0");
+
+            //Validate result
+            page.ValidateFromPoints(dateFormat, FromPoint);
+            page.ValidateToPoints(ToPoint);
+        
+        }
+    }
+}
