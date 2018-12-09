936c01769e4fd2419bc4073ffef0c42b1ae0abef
auhpriem <cacha51484@mail.ru>
2018-12-09 23:22:28 +0300

    Lab 8 Project Type Changing

---
+++ Lab 8 - Framework (Step 2)/Lab 8 - Framework (Step 2)/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab 8 - Framework (Step 2)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab 8 - Framework (Step 2)")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bdc5eee2-0bcd-404b-858c-219b40bbbbdf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
